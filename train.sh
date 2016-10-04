7a9bc12ae0e9a2d3d7039b7d2122779dd52d4e1b
Yuandong Tian <yuandong@fb.com>
2016-10-04 13:12:52 -0700

    Add licensing information

--- train.sh
+++ train.sh
@@ -1,2 +1,11 @@
+#
+# Copyright (c) 2016-present, Facebook, Inc.
+# All rights reserved.
+#
+# This source code is licensed under the BSD-style license found in the
+# LICENSE file in the root directory of this source tree. An additional grant
+# of patent rights can be found in the PATENTS file in the same directory.
+#
+
 #!/bin/bash -e
  th train.lua --nGPU 1 --datasource gogod --num_forward_models 2048 --alpha 0.05 --epoch_size 128000 --data_augmentation
--- train/rl_framework/examples/go/ParallelCriterion2.lua
+++ train/rl_framework/examples/go/ParallelCriterion2.lua
@@ -1,3 +1,12 @@
+--
+-- Copyright (c) 2016-present, Facebook, Inc.
+-- All rights reserved.
+--
+-- This source code is licensed under the BSD-style license found in the
+-- LICENSE file in the root directory of this source tree. An additional grant
+-- of patent rights can be found in the PATENTS file in the same directory.
+--
+
 local ParallelCriterion, parent = torch.class('nn.ParallelCriterion2', 'nn.Criterion')
 
 function ParallelCriterion:__init(rep_count)
@@ -5,7 +14,7 @@ function ParallelCriterion:__init(rep_count)
    self.criterions = {}
    self.weights = {}
    self.gradInput = {}
-   -- If rep_count == 2, and #criterions = 10, we expect 5 targets. 
+   -- If rep_count == 2, and #criterions = 10, we expect 5 targets.
    -- The 10 criterions will receive the following targets in order: t1, t2, t3, t4, t5,    t1, t2, t3, t3, t5
    self.rep_count = rep_count or 1
 end
@@ -19,7 +28,7 @@ end
 
 function ParallelCriterion:updateOutput(input, target)
    self.output = 0
-   assert(#self.criterions == target:size(2) * self.rep_count, 
+   assert(#self.criterions == target:size(2) * self.rep_count,
           string.format("ParallelCriterion2: #criterions [%d] != #target [%d] * #repcount [%d]", #self.criterions, target:size(2), self.rep_count))
    for i,criterion in ipairs(self.criterions) do
       -- Target size is nbatch x #targets, which is more suitable for torchnet setting.
