daee5bc57e1bd3512d6ef2397f95a9772805f88d
Yuandong Tian <yuandong.tian@gmail.com>
2016-10-17 18:52:27 -0700

    Change default parameters in training.

--- train.sh
+++ train.sh
@@ -8,4 +8,4 @@
 #
 
 #!/bin/bash -e
- th train.lua --nGPU 1 --datasource gogod --num_forward_models 2048 --alpha 0.05 --epoch_size 128000 --data_augmentation
+th train.lua --nGPU 1 --datasource gogod --num_forward_models 2048 --nthread 4 --alpha 0.05 --epoch_size 128000 --data_augmentation --batchsize 128
